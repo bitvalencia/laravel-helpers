55e797f1482625e75958c50e365e30f302b8f55c
IvanG11 <IvanG11@users.noreply.github.com>
2025-05-28 16:51:33 +0000

    Fix styling

--- src/Helpers.php
+++ src/Helpers.php
@@ -91,8 +91,8 @@ if (! function_exists('money')) {
 
         $numberOfDecimalPlaces = $showCents ? 2 : 0;
 
-        $formatter = numfmt_create('en_US', \NumberFormatter::CURRENCY);
-        $formatter->setAttribute(\NumberFormatter::FRACTION_DIGITS, $numberOfDecimalPlaces);
+        $formatter = numfmt_create('en_US', NumberFormatter::CURRENCY);
+        $formatter->setAttribute(NumberFormatter::FRACTION_DIGITS, $numberOfDecimalPlaces);
 
         return numfmt_format_currency($formatter, $input, trim(localeconv()['int_curr_symbol']));
     }
